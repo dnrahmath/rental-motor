c31a771bcbff65d3082caac64c92049f51497edf
dnrahmath <50563545+dnrahmath@users.noreply.github.com>
2022-11-22 07:53:15 +0700

    update fix my.ini

--- koneksi.php
+++ koneksi.php
@@ -9,4 +9,18 @@ $conn = mysqli_connect($server, $username, $password, $database, $port);
 if (!$conn) {
     die('Gagal terhubung: ' . mysqli_connect_error());
 }
+
+
+
+//CREATE USER 'rahmat'@'localhost' IDENTIFIED BY 'mypassword';
+//GRANT ALL PRIVILEGES ON * . * TO 'rahmat'@'localhost';
+//FLUSH PRIVILEGES;
+
+// xampp -> mysql -> my.ini
+//[mysqld]
+//skip-grant-tables
+//port=3306
+
+
+
 ?>
